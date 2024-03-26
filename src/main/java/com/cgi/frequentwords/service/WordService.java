be884725eb125c97fc4f4696f760c33afa007064
Teeceeooo <130836375+Teeceeooo@users.noreply.github.com>
2024-03-26 12:04:40 +0100

    Update punctuation v2
    Fixed a new punctuation issue where .split("\\W+") caused Swedish letters to be counted faulty.
    
    Updated to .split("[^\\p{L}\\p{Nd}]+") which matches words in all languages and matches all decimals in Unicode standard.

--- src/main/java/com/cgi/frequentwords/service/WordService.java
+++ src/main/java/com/cgi/frequentwords/service/WordService.java
@@ -10,8 +10,8 @@ import java.util.stream.Collectors;
 public class WordService {
 
     public Map<String, Long> getTopFrequentWords(String text, int limit) {
-        List<String> words = Arrays.asList(text.toLowerCase().split("\\W+"));
-
+        List<String> words = Arrays.asList(text.toLowerCase().split("[^\\p{L}\\p{Nd}]+"));
+        
         Map<String, Long> wordCount = words.stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
